e0a4289773ab8fe6550df341eb7f3e397533eecb
Luis Miguel <luiscondori023@gmail.com>
2024-06-05 05:51:44 -0500

    Se agrega el proyecto

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+    <div class="clearfix"></div>
+</div>
+
+    <footer id="pie">
+        <p>Desarrollado por Luis Miguel &copy; 2024</p>
+    </footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,69 @@
+
+<?php require_once 'includes/cabecera.php'; ?>
+    
+<?php require_once 'includes/lateral.php'; ?>
+    
+
+    <!-- BARRA LATERAL -->
+
+    <!-- CAJA PRINCIPAL -->
+    <div id="principal">
+        <h1>Ultimas entradas</h1>
+
+        <article class="entradas">
+            <a href="">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi quos, qui, harum corrupti, a quam vel velit sed voluptatem aut nam tempore deleniti at sint laborum. Quae, libero. Quo, dolor!
+                </p>
+            </a>
+        </article>
+
+        <article class="entradas">
+            <a href="">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi quos, qui, harum corrupti, a quam vel velit sed voluptatem aut nam tempore deleniti at sint laborum. Quae, libero. Quo, dolor!
+                </p>
+            </a>
+        </article>
+
+        <article class="entradas">
+            <a href="">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi quos, qui, harum corrupti, a quam vel velit sed voluptatem aut nam tempore deleniti at sint laborum. Quae, libero. Quo, dolor!
+                </p>
+            </a>
+        </article>
+
+        <article class="entradas">
+            <a href="">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi quos, qui, harum corrupti, a quam vel velit sed voluptatem aut nam tempore deleniti at sint laborum. Quae, libero. Quo, dolor!
+                </p>
+            </a>
+        </article>
+
+        <article class="entradas">
+            <a href="">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi quos, qui, harum corrupti, a quam vel velit sed voluptatem aut nam tempore deleniti at sint laborum. Quae, libero. Quo, dolor!
+                </p>
+            </a>
+            
+        </article>
+
+        <div id="ver-todas">
+            <a href="">Ver todas las entradas</a>
+        </div>
+    </div><!-- fin principal -->
+
+    
+
+
+
+<?php require_once 'includes/footer.php'; ?>
+    
